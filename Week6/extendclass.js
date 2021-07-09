f2e5e104f6cc62a5e87c5138b115ab113729ce0e
wisdom2301 <85389008+wisdom2301@users.noreply.github.com>
2021-07-09 11:19:31 +0100

    Week 6 Update

--- Week6/extendclass.js
+++ Week6/extendclass.js
@@ -1,10 +1,27 @@
+const ul = document.getElementById('users');
 
-fetch('https://randomuser.me/api/?results=10')
+fetch('https://randomuser.me/api/?results=15')
 .then((resp) => resp.json())
 .then (function (data){
-    console.log(data);
-    console.log(data.results);
+    let users = data.results;
+    return users.map((user) => {
+
+        const li = document.createElement('li');
+        
+        const img = document.createElement('img');
+        const p = document.createElement('p');
+        ul.appendChild(li);
+
+        let paragraph = li.appendChild(p);
+        let image = li.appendChild(img);
+        let firstName = user.name.first;
+        let lastName = user.name.last;
+
+        paragraph.innerHTML = `${firstName} ${lastName}`;
+        image.src = user.picture.large;
+        
+    });
     })
 .catch(function (error) {
     console.log(error);
-});
+}); 
